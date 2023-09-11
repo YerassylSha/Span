e78e6f5dff038e2619bf7b6a005a030b5f518fea
YerassylSha <yyerassyl@gmail.com>
2023-09-11 11:41:41 +0200

    main_window

---
+++ Arnion/ui/main_window.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+
+class MainWindow:
+
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("310x380")
+        self.window.title("SPAN")
+
+
+
+        def start_mainloop(self):
+            self.window.mainloop()
+
+    def start_mainloop(self):
+        pass
---
+++ main.py
@@ -0,0 +1,12 @@
+from Arnion.ui.main_window import MainWindow
+
+
+def main():
+    main_window = MainWindow()
+    main_window.start_mainloop()
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
